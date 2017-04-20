34d590c75690f25b01745314a53eaf182cc534ec
zhaoxny <Administrator@10.16.139.28>
2017-04-20 09:23:56 +0800

    test

---
+++ shipInspection/src/com/boyan/ship/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.boyan.ship.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class LoginController {
+
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(String name,String password){
+		return "/main";
+	}
+}
